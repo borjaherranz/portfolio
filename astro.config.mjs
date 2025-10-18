1daad071cd1abf7df6718e68862fff85d5eeb7d3
borja <borjahz92@gmail.com>
2025-10-18 15:01:32 +0200

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://borjaherranz.github.io/',
-    base: '/portfolio/'
+    base: '/portfolio/',
+    vite: {
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src')
+            }
+        }
+    }
 });
